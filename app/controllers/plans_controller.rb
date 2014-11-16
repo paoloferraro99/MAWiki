35ba5420b11d561c6bf1339d98a33d1f72e46a3e
paoloferraro99 <paoloferraro99@gmail.com>
2014-11-16 23:15:53 +0000

    plans controller setup

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -18,9 +18,9 @@ class PlansController < ApplicationController
 
 def check_membership
 
-  if current_user.role == 'premium'
-    flash[:notice] = "Congratulations, you are a premium member."
-    redirect_to wikis_path
+  if current_user && current_user.role == 'premium'
+    # flash[:notice] = "Congratulations, you are a premium member."
+    redirect_to wikis_path, notice: "Congratulations, you are a premium member!"
   elsif current_user
     redirect_to new_charge_path
   elsif current_user == nil
