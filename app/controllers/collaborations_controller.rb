92510699d3aa68c90f6b471bc9e23fab53479f27
paoloferraro99 <paoloferraro99@gmail.com>
2014-12-02 06:58:50 +0000

    ajax halfway

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -9,10 +9,15 @@ class CollaborationsController < ApplicationController
       user = User.where(:email => params[:collaboration][:user_email]).first
       if user.present? && @wiki.collaborations.where(:user_id => user.id).first.blank?
         @wiki.users << user
+        flash[:notice] = "Collaborator successfully added."
+      elsif !@wiki.collaborations.where(:user_id => user.id).first.blank?
+        flash[:error] = "They are already a collaborator.  Try again please."
+      else
+        flash[:error] = "Collaborator not found.  Try again please."
       end
     end
-
-    redirect_to @wiki, notice: 'Successfully added collaborators' 
+    redirect_to edit_wiki_path(@wiki)
+    # redirect_to @wiki
   end    
 
 
@@ -20,13 +25,14 @@ class CollaborationsController < ApplicationController
     @collaboration = Collaboration.find(params[:id])
     if @collaboration.destroy
       flash[:notice] = "Collaborator successfully removed."
-      # redirect_to @collaboration.wiki
+      #redirect_to edit_wiki_path(@collaboration.wiki)
+      ## redirect_to @collaboration.wiki
     else
       flash[:error] = "Collaborator not removed.  Try again please,"
     end
 
     respond_with(@collaboration) do |format|
-      format.html { redirect_to @collaboration }
+      format.html { redirect_to edit_wiki_path(@collaboration.wiki) }
     end
 
   end
