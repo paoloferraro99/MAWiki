c091ee59424f096d407f0dd30eb608417ae9ef2c
paoloferraro99 <paoloferraro99@gmail.com>
2014-12-03 19:39:06 +0000

    ajax on hold, finish style

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,6 +1,6 @@
 class CollaborationsController < ApplicationController
 
-  respond_to :html, :js
+  ### respond_to :html, :js
   
   def create
     @wiki = Wiki.find(params[:collaboration][:wiki_id])
@@ -10,14 +10,19 @@ class CollaborationsController < ApplicationController
       if user.present? && @wiki.collaborations.where(:user_id => user.id).first.blank?
         @wiki.users << user
         flash[:notice] = "Collaborator successfully added."
-      elsif !@wiki.collaborations.where(:user_id => user.id).first.blank?
+      elsif user.present? && !@wiki.collaborations.where(:user_id => user.id).first.blank?
         flash[:error] = "They are already a collaborator.  Try again please."
       else
         flash[:error] = "Collaborator not found.  Try again please."
       end
     end
     redirect_to edit_wiki_path(@wiki)
-    # redirect_to @wiki
+    ## redirect_to @wiki
+
+    # AJAX _____
+    # respond_with(@comment) do |format|
+    #   format.html { redirect_to edit_wiki_path(@wiki) }
+    # end
   end    
 
 
@@ -25,15 +30,16 @@ class CollaborationsController < ApplicationController
     @collaboration = Collaboration.find(params[:id])
     if @collaboration.destroy
       flash[:notice] = "Collaborator successfully removed."
-      #redirect_to edit_wiki_path(@collaboration.wiki)
+      redirect_to edit_wiki_path(@collaboration.wiki)
       ## redirect_to @collaboration.wiki
     else
       flash[:error] = "Collaborator not removed.  Try again please,"
     end
 
-    respond_with(@collaboration) do |format|
-      format.html { redirect_to edit_wiki_path(@collaboration.wiki) }
-    end
+    # AJAX _____
+    # respond_with(@collaboration) do |format|
+    #   format.html { redirect_to edit_wiki_path(@collaboration.wiki) }
+    # end
 
   end
 
